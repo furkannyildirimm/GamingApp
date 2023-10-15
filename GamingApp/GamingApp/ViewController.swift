d327f6e0b4de774ee80526ec54f92d7fb705a9ab
furkannyildirimm <furkannyildirimm@hotmail.com>
2023-10-15 19:13:34 +0300

    firstCommit

---
+++ GamingApp/GamingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GamingApp
+//
+//  Created by STARK on 15.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
